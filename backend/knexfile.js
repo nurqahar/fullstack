069fefaed014849f17dc870f735847f31a20fcec
qahar <adha.nur.qahar@gmail.com>
2025-10-30 15:14:12 +0700

    change knexfile.js backend

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,41 +7,15 @@ export default {
   development: {
     client: "mysql",
     connection: {
-      host: process.env.HOST,
-      port: process.env.PORT,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE_NAME,
+      host: process.env.host,
+      port: process.env.port,
+      user: process.env.user,
+      password: process.env.password,
+      database: process.env.database,
     },
     migrations: {
       tableName: "knex_migrations",
       directory: "./migrations/development",
-    },
-  },
-
-  staging: {
-    client: "postgresql",
-    connection: {
-      host: process.env.HOST,
-      port: process.env.PORT,
-      user: process.env.USER,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE_NAME,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./migrations/staging",
-    },
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./migrations/production",
     },
   },
 };
