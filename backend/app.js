712919ad3c35d50f4c37927b1bbf81c56ad04d6c
qahar <adha.nur.qahar@gmail.com>
2025-11-06 09:08:33 +0700

    backend - add swagger

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/indexRoute.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 const port = 3000;
@@ -8,6 +10,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`API url: http://localhost:${port}/api/`);
